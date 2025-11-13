3d25a1347f5ac33e0f2adf9da94309fb20e12dae
Muhammad Ijaz <epassionate01@gmail.com>
2025-11-13 19:58:08 +0500

    First component created

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,18 @@
+export const Welcome = () => {
+  return (
+    <h3>Welcome to Pursuit Medical Center</h3>
+  )
+}
+
+// export default Welcome;
+
+
+
+
+// function Welcome() {
+//   return (
+//     <h3>Welcome to Pursuit Medical Center</h3>
+//   )
+// }
+
+// export default Welcome;
